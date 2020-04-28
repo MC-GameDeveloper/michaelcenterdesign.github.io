cc4890535d3888a6d5fa2fddc204cda79e90e891
mtc9417 <mtc9417@gmail.com>
2020-04-28 19:23:28 -0400

    cleaning

--- js/shapes.js
+++ js/shapes.js
@@ -18,7 +18,7 @@ function main() {
     const fov = 45;
     const aspect = 2;  // the canvas default
     const near = 0.1;
-    const far = 5;
+    const far = 50;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 0, 2);
     camera.lookAt(0, 0, 0);
